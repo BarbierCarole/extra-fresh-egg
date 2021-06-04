46c44a2567063c8dfb3c7f79029492ef2601b8f1
BarbierCarole <carole83460@gmail.com>
2021-06-04 11:13:10 +0200

    img + modif

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Moment from 'react-moment';
 import 'moment/locale/fr';
 import moment from 'moment';
 import { useForm } from './useForm';
+import eggs from './img/eggs.png';
 
 function App() {  
  
@@ -20,6 +21,8 @@ function App() {
   const dateLimiteExtra = moment(dcr).subtract(20,'days');
   const [showExtra, setShowExtra] = useState(false);
   const dateIsValid = moment(dcr).isValid();
+
+  console.log('Image par <a href="https://pixabay.com/fr/users/vipbum-3582769/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3070850">Hung Nguyen</a> de <a href="https://pixabay.com/fr/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3070850">Pixabay</a>');
  
   useEffect(() => {
     if (moment(today).isBefore(dateLimiteExtra)) {
@@ -31,6 +34,7 @@ function App() {
     
       <div className="App">
         <header className="App-header">
+          <img src={eggs} width="40%"/>
           <h1>Mes &#339;ufs sont-ils extra-frais ?</h1>
           <p>Veuillez saisir la date de consommation recommandée (DCR)<br />
           La DCR est la date écrite sur l'&#339;uf</p>
@@ -42,9 +46,9 @@ function App() {
           <label htmlFor="year">Année </label>
           <input type="number" id="year" name="year" min="2021" max="2040" value={values.year} placeholder="année" onChange={handleChange} /><br />
 
-          { dateIsValid ? <div>La date de consommation recommandée est le <Moment format="DD MM YYYY">{dcr}</Moment></div> : ""}
+          {/* { dateIsValid ? <div>La date de consommation recommandée est le <Moment format="DD MM YYYY">{dcr}</Moment></div> : ""} */}
 
-          {showExtra ? <div> Mon &#339;uf est extra frais du <Moment format="D MMM YYYY">{datePonte}</Moment> au <Moment format="D MMM YYYY">{dateLimiteExtra}</Moment></div>   : "Mon œuf n'est plus extra-frais '("}<br />
+          {showExtra ? <div> Mon &#339;uf est extra frais du <br/><Moment format="D MMM YYYY">{datePonte}</Moment> au <Moment format="D MMM YYYY">{dateLimiteExtra}</Moment></div>   : "Mon œuf n'est plus extra-frais '("}<br />
           <p>Nous sommes le <Moment format="D MMM YYYY"></Moment> </p>
         </header>       
       </div>      
